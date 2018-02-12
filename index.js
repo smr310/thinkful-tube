bf0b90672c48a038d910b17d7986ceb3472a1baa
Steve Romm <smr310@gmail.com>
2018-02-11 21:43:14 -0500

    first attempt

---
+++ index.js
@@ -0,0 +1,52 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function pageLoadCallBack() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+
+    getDataFromApi(query, getDataFromApiCB);
+  });
+}
+
+function getDataFromApi(searchTerm, callback) {
+  const query = {
+    q: searchTerm,
+    per_page: 5,
+    part: 'snippet',
+    key: "AIzaSyDW01WDj_JY47WKZmAJ14fj7TXaiM-nOZM"
+
+  }
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+function getDataFromApiCB(data) {
+  console.log(data);
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
+}
+
+function renderResult(eachItem) {
+  return `
+    <div class='results'>
+      ${eachItem.snippet.title}
+      <img data-item-id="${eachItem.id.videoId}" onclick="clickCB(event)" class="image" src="${eachItem.snippet.thumbnails.medium.url}">
+    </div>
+  `;
+}
+
+function clickCB(event) {
+  const videoID = $(event.target).attr("data-item-id")
+  window.location.href = `https://www.youtube.com/watch?v=${videoID}`;
+};
+
+// const thumbnail = document.querySelectorAll('.image');
+// thumbnail.addEventListener("click", function() {
+//   console.log('event listener working');
+// });
+
+
+$(pageLoadCallBack);
